f8562342fdc65d90b109d511a3a77058a56b6692
Matthew Bondad <matthewbondad@gmail.com>
2025-07-01 02:01:51 +0800

    Initial server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import indexRoute from "./routes/index.js";
+import postsRoute from "./routes/posts.js";
+
+const app = express();
+const PORT = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRoute);
+app.use("/posts", postsRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
